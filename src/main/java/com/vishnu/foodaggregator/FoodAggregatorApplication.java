2c22576bec406e6e75df886c4da2692a9177459b
Vishnu Govindan <vishnugovindan@gmail.com>
2020-07-11 11:36:22 +0530

    Create a spring boot project with the dependencies needed for a starting point. Closes #1

---
+++ src/main/java/com/vishnu/foodaggregator/FoodAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.vishnu.foodaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vishnu/foodaggregator/FoodAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.foodaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
